0936dd427872ce0590b9838157d538a5212c73ef
Vengerov Dmitrij <hardbeat34@gmail.com>
2017-11-10 00:31:58 +0300

    with pouch

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const NodeCouchDb = require('node-couchdb');
 
-// connect to couch db
 const couch = new NodeCouchDb({
 	auth: {
 		user:     'admin',
@@ -13,10 +12,14 @@ const couch = new NodeCouchDb({
 
 const dbname = 'todolist';
 const viewUrl = '_design/all_list/_view/all';
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'js')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -25,58 +28,36 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', function(req, res){
 	couch.get(dbname, viewUrl).then(
 		function(data, headers, status){
-			//console.log(data.data.rows);
+			console.log(data.data.rows);
 			res.render('index',{ todolist: data.data.rows });
 		},
 		function(err){
-			// error
 			res.send(err);
 		});
 });
 
 app.post('/list/add', function(req, res){
-
-	// date from form
 	const text = req.body.text;
-
-	// couch.uniqid().then(ids => ids[0]);
 	couch.uniqid().then(function(ids){
 		const id = ids[0];
-
-		/*
-			couch.insert("databaseName", {
-    		_id: "document_id",
-    		field: ["sample", "data", true]
-    	*/
-
-		couch.insert(dbname,{ _id:id, text:text }).then(
-
-			function(data,headers,status){
-				// redirect 
+		couch.insert(dbname,{ _id:id, title:text }).then(
+			function(data,headers,status){ 
 				res.redirect('/');
 			},
 			function(err){
-				// error
 				res.send(err);
 			});
 	});
 });
 
 app.post('/list/delete/:id', function(req, res){
-
-	// date from form 
 	const id = req.params.id;
-	console.log(id);
 	const rev = req.body.rev;
-
-	// couch.del("databaseName", "some_document_id", "document_revision") with promise
 	couch.del(dbname,id,rev).then(
 		function(data,headers,status){
-			// redirect 
 			res.redirect('/');
 		},
 		function(){
-			// error
 			res.send(err);
 		});
 });
